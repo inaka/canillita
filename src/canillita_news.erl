176227cf02f655db497fec098be0d3290c805fed
Marcos Almonacid <marcos@inaka.net>
2014-02-24 13:31:41 -0300

    added record: canillita_new

--- src/canillita_news.erl
+++ src/canillita_news.erl
@@ -4,6 +4,15 @@
 
 -behaviour(sumo_doc).
 
+-record(canillita_new,
+  { id          :: pos_integer()
+  , title       :: binary()
+  , content     :: binary()
+  , created_at  :: canillita_utils:datetime()
+  , updated_at  :: canillita_utils:datetime()
+  }).
+
+
 %%% Public API
 -export(
   [ new/2
@@ -27,9 +36,24 @@ sumo_schema() ->
     , sumo:new_field(updated_at,    datetime, [not_null])
     ]).
 
-sumo_sleep(NewsFlash) -> NewsFlash.
+-spec sumo_sleep(#canillita_new{}) -> proplists:proplists().
+sumo_sleep(NewsFlash) ->
+  [ {id,          NewsFlash#canillita_new.id}
+  , {title,       NewsFlash#canillita_new.title}
+  , {content,     NewsFlash#canillita_new.content}
+  , {created_at,  NewsFlash#canillita_new.created_at}
+  , {updated_at,  NewsFlash#canillita_new.updated_at}
+  ].
 
-sumo_wakeup(NewsFlash) -> NewsFlash.
+-spec sumo_wakeup(proplists:proplists()) -> #canillita_new{}.
+sumo_wakeup(NewsFlash) -> 
+  #canillita_new{
+    id         = proplists:get_value(id, NewsFlash),
+    title      = proplists:get_value(title, NewsFlash),
+    content    = proplists:get_value(content, NewsFlash),
+    created_at = proplists:get_value(created_at, NewsFlash),
+    updated_at = proplists:get_value(updated_at, NewsFlash)
+  }.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% PUBLIC API
@@ -37,17 +61,18 @@ sumo_wakeup(NewsFlash) -> NewsFlash.
 %% @doc Creates, stores and returns a news flash.
 new(Title, Content) ->
   Now = {datetime, calendar:universal_time()},
-  NewsFlash =
-    [ {title,       Title}
-    , {content,     Content}
-    , {created_at,  Now}
-    , {updated_at,  Now}],
+  NewsFlash = #canillita_new{
+    title      =  Title,
+    content    =  Content,
+    created_at =  Now,
+    updated_at =  Now
+  },
   sumo:persist(canillita_news, NewsFlash).
 
-get_id(NewsFlash) -> proplists:get_value(id, NewsFlash).
+get_id(NewsFlash) -> NewsFlash#canillita_new.id.
 
-get_title(NewsFlash) -> proplists:get_value(title, NewsFlash).
+get_title(NewsFlash) -> NewsFlash#canillita_new.title.
 
-get_content(NewsFlash) -> proplists:get_value(content, NewsFlash).
+get_content(NewsFlash) -> NewsFlash#canillita_new.content.
 
 latest_news() -> lists:reverse(sumo:find_all(canillita_news)).
---
+++ src/canillita_utils.erl
@@ -0,0 +1,5 @@
+-module(canillita_utils).
+
+-type datetime() :: {datetime,{{integer(),integer(),integer()},{integer(),integer(),integer()}}}.
+
+-export_type([datetime/0]).
