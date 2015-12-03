bc785449cca23f8943b89322f974f33ca9c268b0
Harenson Henao <harenson@gmail.com>
2015-12-03 18:54:00 -0500

    [#6] Add application behaviour callbacks

--- src/canillita_app.erl
+++ src/canillita_app.erl
@@ -1,11 +1,32 @@
 -module(canillita_app).
 -behaviour(application).
 
--export([start/2]).
--export([stop/1]).
+-export([start/0, start/2, start_phase/3]).
+-export([stop/0, stop/1]).
 
-start(_Type, _Args) ->
-  canillita_sup:start_link().
+%% @doc Starts the Application
+-spec start() -> {ok, [atom()]} | {error, term()}.
+start() -> {ok, _} = application:ensure_all_started(canillita).
 
-stop(_State) ->
-  ok.
+%% @doc Stops the Application
+-spec stop() -> ok | {error, term()}.
+stop() -> ok = application:stop(canillita).
+
+-spec start(Type::application:start_type(), Args::any()) -> {ok, pid()}.
+start(_Type, _Args) -> {ok, self()}.
+
+-spec stop(State::[]) -> ok.
+stop(_State) -> ok.
+
+%% @private
+-spec start_phase(atom(), StartType::application:start_type(), []) ->
+  ok | {error, _}.
+start_phase(create_schema, _StartType, []) ->
+  _ = application:stop(mnesia),
+  Node = node(),
+  case mnesia:create_schema([Node]) of
+    ok -> ok;
+    {error, {Node, {already_exists, Node}}} -> ok
+  end,
+  {ok, _} = application:ensure_all_started(mnesia),
+  sumo:create_schema().
