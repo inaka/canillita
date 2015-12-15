6a7e1be55d06e5b58ff6bfa30d656654ff2f147c
Harenson Henao <harenson@gmail.com>
2015-12-15 15:39:56 -0500

    [#10] Add last_event_id_test/1 test and improve news_api_test/1 test

--- test/canillita_news_SUITE.erl
+++ test/canillita_news_SUITE.erl
@@ -12,7 +12,7 @@
         , end_per_testcase/2
         ]).
 
--export([news_api_test/1]).
+-export([news_api_test/1, last_event_id_test/1]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% common tests
@@ -47,32 +47,126 @@ news_api_test(_Config) ->
   Newspaper = canillita_test_utils:create_newspaper( <<"newspaper1">>,
                                                       <<"description1">>
                                                     ),
+  NewspaperBin = list_to_binary(Newspaper),
+  Newspaper2 = canillita_test_utils:create_newspaper( <<"newspaper2">>
+                                                    , <<"description2">>
+                                                    ),
+  NewspaperBin2 = list_to_binary(Newspaper2),
+  % open SSE connection
   AsyncPid = canillita_test_utils:async_api_call("/news", Headers),
-  ok = try
-    % create a newsitem
-    Body1 = #{<<"title">> => <<"title1">> , <<"body">> => <<"body1">>},
-    #{status_code := 201, body := RespBody1} =
-      canillita_test_utils:create_newsitem(Newspaper, Headers, Body1),
-    Id1 = maps:get(<<"id">>, sr_json:decode(RespBody1)),
-    % Wait the event replication
-    timer:sleep(300),
-    [{_, _, EventBin1}] = shotgun:events(AsyncPid),
-    Id1 = maps:get(id, shotgun:parse_event(EventBin1)),
+  % create a newsitem
+  Body1 = #{<<"title">> => <<"title1">> , <<"body">> => <<"body1">>},
+  #{status_code := 201, body := RespBody1} =
+    canillita_test_utils:create_newsitem(Newspaper, Headers, Body1),
+  Id1 = maps:get(<<"id">>, sr_json:decode(RespBody1)),
+  % check it matches the expected response
+  ParsedEvent1 =
+    #{ id => Id1
+     , event => NewspaperBin
+     , data => [<<"title1">>, <<"body1">>]
+     },
+  ParsedEvent1 =
+    ktn_task:wait_for(
+      fun() ->
+          [{_, _, EventBin1}] = shotgun:events(AsyncPid)
+        , shotgun:parse_event(EventBin1)
+      end, ParsedEvent1, 100, 10
+    ),
+  Id1 = maps:get(id, ParsedEvent1),
 
-    % create a new newsitem
-    Body2 = #{<<"title">> => <<"title2">> , <<"body">> => <<"body2">>},
-    #{status_code := 201, body := RespBody2} =
-      canillita_test_utils:create_newsitem(Newspaper, Headers, Body2),
-    Id2 = maps:get(<<"id">>, sr_json:decode(RespBody2)),
-    % Wait the event replication
-    timer:sleep(300),
-    [{_, _, EventBin2}] = shotgun:events(AsyncPid),
-    Id2 = maps:get(id, shotgun:parse_event(EventBin2)),
-    ok
-  catch
-    _:Exception -> throw({error, Exception})
-  after
-    shotgun:close(AsyncPid)
-  end,
+  % create a new newsitem
+  Body2 = #{<<"title">> => <<"title2">> , <<"body">> => <<"body2">>},
+  #{status_code := 201, body := RespBody2} =
+    canillita_test_utils:create_newsitem(Newspaper, Headers, Body2),
+  Id2 = maps:get(<<"id">>, sr_json:decode(RespBody2)),
+  % check it matches the expected response
+  ParsedEvent2 =
+    #{ id => Id2
+     , event => NewspaperBin
+     , data => [<<"title2">>, <<"body2">>]
+     },
+  ParsedEvent2 =
+    ktn_task:wait_for(
+      fun() ->
+          [{_, _, EventBin2}] = shotgun:events(AsyncPid)
+        , shotgun:parse_event(EventBin2)
+      end, ParsedEvent2, 100, 10
+    ),
+  Id2 = maps:get(id, ParsedEvent2),
+
+  %create a new newsitem for a different newspaper
+  Body3 = #{<<"title">> => <<"title3">>, <<"body">> => <<"body3">>},
+  #{status_code := 201, body := RespBody3} =
+    canillita_test_utils:create_newsitem(Newspaper2, Headers, Body3),
+  Id3 = maps:get(<<"id">>, sr_json:decode(RespBody3)),
+  % and check it is also been notified
+  ParsedEvent3 =
+    #{ id => Id3
+     , event => NewspaperBin2
+     , data => [<<"title3">>, <<"body3">>]
+     },
+  ParsedEvent3 =
+    ktn_task:wait_for(
+      fun() ->
+          [{_, _, EventBin3}] = shotgun:events(AsyncPid)
+        , shotgun:parse_event(EventBin3)
+      end, ParsedEvent3, 100, 10
+    ),
+  Id3 = maps:get(id, ParsedEvent3),
+
+  % close shotgun connection
+  shotgun:close(AsyncPid),
+
+  {comment, ""}.
+
+-spec last_event_id_test(Config::canillita_test_utils:config()) ->
+  {comment, string()}.
+last_event_id_test(_Config) ->
+  Headers = #{<<"content-type">> => <<"application/json; charset=utf-8">>},
+  % Every newsitem needs to be attached to an existing newspaper
+  Newspaper = canillita_test_utils:create_newspaper( <<"newspaper1">>,
+                                                      <<"description1">>
+                                                    ),
+  NewspaperBin = list_to_binary(Newspaper),
+
+  % create a newsitem0
+  Body0 = #{<<"title">> => <<"title0">> , <<"body">> => <<"body0">>},
+  #{status_code := 201, body := _RespBody0} =
+    canillita_test_utils:create_newsitem(Newspaper, Headers, Body0),
+  % create a newsitem1
+  Body1 = #{<<"title">> => <<"title1">> , <<"body">> => <<"body1">>},
+  #{status_code := 201, body := RespBody1} =
+    canillita_test_utils:create_newsitem(Newspaper, Headers, Body1),
+  Id1 = maps:get(<<"id">>, sr_json:decode(RespBody1)),
+
+  % create header for getting just the events from now on
+  LastEventIdHeaders = Headers#{<<"last-event-id">> => Id1},
+  % open SSE connection with last-event-id header set
+  AsyncPid = canillita_test_utils:async_api_call("/news", LastEventIdHeaders),
+
+  % create a newsitem2
+  Body2 = #{<<"title">> => <<"title2">> , <<"body">> => <<"body2">>},
+  #{status_code := 201, body := RespBody2} =
+    canillita_test_utils:create_newsitem(Newspaper, Headers, Body2),
+  Id2 = maps:get(<<"id">>, sr_json:decode(RespBody2)),
+
+  % check it matches the expected response
+  ParsedEvent2 =
+    #{ id => Id2
+     , event => NewspaperBin
+     , data => [<<"title2">>, <<"body2">>]
+     },
+  % only the last newsitem must be returned
+  ParsedEvent2 =
+    ktn_task:wait_for(
+      fun() ->
+          [{_, _, EventBin2}] = shotgun:events(AsyncPid)
+        , shotgun:parse_event(EventBin2)
+      end, ParsedEvent2, 100, 10
+    ),
+  Id2 = maps:get(id, ParsedEvent2),
+
+  % close SSE connection
+  shotgun:close(AsyncPid),
 
   {comment, ""}.
