ab141cfcc69830d96c58fb7317ef34169a1ae3d3
Harenson Henao <harenson@gmail.com>
2015-12-15 22:53:39 -0500

    [#10] Remove macros from newsitems repo

--- src/canillita_newsitems_repo.erl
+++ src/canillita_newsitems_repo.erl
@@ -3,12 +3,10 @@
 
 -export([fetch/1, fetch/2, fetch_since/1]).
 
--define(TABLE, canillita_newsitems).
-
 %% @doc returns the newsitem identified with the given id.
 -spec fetch(Id::canillita_newsitems:id()) -> notfound | sumo_rest_doc:entity().
 fetch(Id) ->
-  sumo:find(?TABLE, Id).
+  sumo:find(canillita_newsitems, Id).
 
 %% @doc Returns the newsitem that matches the given
 %%      newspaper_name and id (if any).
@@ -17,7 +15,7 @@ fetch(Id) ->
            ) -> notfound | sumo_rest_doc:entity().
 fetch(NewspaperName, Id) ->
   Conditions = [{id, Id}, {newspaper_name, NewspaperName}],
-  sumo:find_one(?TABLE, Conditions).
+  sumo:find_one(canillita_newsitems, Conditions).
 
 %% @doc returns all the news after the given event-id or all the news
 %%      if not event-id provided.
@@ -36,10 +34,10 @@ fetch_since(LastEventId) ->
 %% @doc returns all the newsitems stored so far.
 -spec fetch_all() -> [sumo_rest_doc:entity()].
 fetch_all() ->
-  sumo:find_all(?TABLE).
+  sumo:find_all(canillita_newsitems).
 
 %% @doc returns all elements created after the given datetime.
 -spec fetch_all(CreatedAt::calendar:datetime()) -> [sumo_rest_doc:entity()].
 fetch_all(CreatedAt) ->
   Conditions = [{created_at, '>', CreatedAt}],
-  sumo:find_by(?TABLE, Conditions).
+  sumo:find_by(canillita_newsitems, Conditions).
